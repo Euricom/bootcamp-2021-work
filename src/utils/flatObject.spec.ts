78c768da90ea6d34f4aac4730a581ed0fcaa6674
TheNoisyMonk <yornethys@hotmail.com>
2021-08-09 21:12:21 +0200

    Recursive function to flatten JSON.

--- src/utils/flatObject.spec.ts
+++ src/utils/flatObject.spec.ts
@@ -1,24 +1,20 @@
 import { Json } from '../contexts/TranslationContext';
 
-const flatObject = (source: Record<string, Json>, path? = '', target: Record<string, string>): Record<string, string> => {
-  console.log(Object.entries(source));
-  const test = Object.entries(source).reduce((prev, cur) => {
-    console.log(cur[0]);
-    if (typeof cur[1] === 'string') return { ...prev, [cur[0]]: cur[1] };
-
-    const currentPath =
-
-    return flatObject(cur[1], );
-  }, {});
-  console.log(test);
-  // const test = Object.entries(source).map((value) => {
-  //   if (typeof value[1] !== 'string') {
-
-  //   }
-  // });
-  // console.log(test);
-  // return source;
+const flatObject = (source: Record<string, Json>): Record<string, string> => {
+  const flattenedJson = {};
+  function recurse (cur: Record<string, Json>, prop: string) {
+    if (Object(cur) !== cur) {
+      flattenedJson[prop] = cur;
+    } else {
+      for (const curKey in cur) {
+        recurse(cur[curKey], prop ? `${prop}.${curKey}` : curKey);
+      }
+    }
+  }
+  recurse(source, "");
+  return flattenedJson;
 };
+
 
 test('it flattens the single level json object', () => {
   const source = { KEY1: 'value1', KEY2: 'value2' };
@@ -26,8 +22,14 @@ test('it flattens the single level json object', () => {
   expect(flatObject(source)).toStrictEqual(source);
 });
 
-test.only('it flattens the multi level json object', () => {
-  const source = { KEY1: 'value1', KEY2: { SKEY1: 'SValue1' } };
+test('it flattens the multi level json object', () => {
+  const source = { KEY1: 'value1', KEY2: { SKEY1: 'SValue1', SKEY2: 'SValue2' } };
 
-  expect(flatObject(source)).toStrictEqual({ KEY1: 'value1', 'KEY2.SKEY1': 'SValue1' });
+  expect(flatObject(source)).toStrictEqual({ KEY1: 'value1', 'KEY2.SKEY1': 'SValue1', 'KEY2.SKEY2': 'SValue2' });
+});
+
+test('it flattens the deeper multi level json object', () => {
+  const source = { KEY1: 'value1', KEY2: { SKEY1: { MSKEY1: 'MSValue1'} } };
+
+  expect(flatObject(source)).toStrictEqual({ KEY1: 'value1', 'KEY2.SKEY1.MSKEY1': 'MSValue1' });
 });
